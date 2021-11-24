89f372bf828053e39d2c46dd10dc2c7f398a2c7d
Jayati Mishra <jmishra_be20@thapar.edu>
2021-11-24 17:53:37 +0530

    added a label

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-   return render(request,'index.html')         ##Creating an index functions that was rendered in urls.py
+   name='Jayati'
+   return render(request,'index.html',{'name': name})         ##Creating an index functions that was rendered in urls.py
 
 
 # Create your views here.
