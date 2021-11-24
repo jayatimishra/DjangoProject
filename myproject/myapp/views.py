f85e626afceac1180e572432e1016954772ac74c
Jayati Mishra <jmishra_be20@thapar.edu>
2021-11-24 14:43:25 +0530

    HTML message to HTTPResponse added

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    pass           ##Creating an index functions that was rendered in urls.py
+   return HttpResponse('<h1><b>Hey there,Welcome!</b></h1>')          ##Creating an index functions that was rendered in urls.py
 
 # Create your views here.
