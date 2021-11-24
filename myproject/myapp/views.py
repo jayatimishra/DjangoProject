3b836129c63febbabbe7c4f5bce3a975c83be576
Jayati Mishra <jmishra_be20@thapar.edu>
2021-11-24 17:32:01 +0530

    request to render index.html in views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,8 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+
 def index(request):
-   return HttpResponse('<h1><b>Hey there,Welcome!</b></h1>')          ##Creating an index functions that was rendered in urls.py
+   return render(request,'index.html')         ##Creating an index functions that was rendered in urls.py
 
 # Create your views here.
