fa555c529c6c507c02985f03d1bff568790b87bc
Jayati Mishra <jmishra_be20@thapar.edu>
2021-11-24 14:47:48 +0530

    Imported HTTPResponse from Django.http

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 def index(request):
    return HttpResponse('<h1><b>Hey there,Welcome!</b></h1>')          ##Creating an index functions that was rendered in urls.py
 
