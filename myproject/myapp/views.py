d0a20b06c93578d61ede76c2e119f6ea3e1b8229
Jayati Mishra <jmishra_be20@thapar.edu>
2022-01-18 12:23:27 +0530

    init

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-   context = { 'name': 'Jayati',
-   'age' : 19, 'nationality' : 'Indian'
-
-   }
-   return render(request,'index.html' , context)          ##Creating an index functions that was rendered in urls.py
+   context = "Temp = {0} *C".format(sensor.read_temperature)
+   return render(request, 'index.html', {"context": context})
 
 
 # Create your views here.
-##Render request index.html in views.py because views.py knows where to look for the index.html (in the templates folder)
+##Render request index.html in views.py because views.py knows where to look for the index.html (in the templates folder)"Temp = {0} *C".format(sensor.read_temperature)
