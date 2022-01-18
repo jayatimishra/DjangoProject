63b33ab0c93561e9efc1b9d02d6e3227f8c0c350
Jayati Mishra <jmishra_be20@thapar.edu>
2022-01-18 18:12:41 +0530

    models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -16,7 +16,7 @@ class Question(models.Model):
     question_text=models.CharField(max_length=200)
     pub_date=models.DateTimeField('date published')
 
-class Choice(odels.Model):
+class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=CASCADE)
     choice_text=models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
