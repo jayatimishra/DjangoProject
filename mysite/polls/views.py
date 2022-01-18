aa2ed4794d4fa0ee3e646438ead962eb91b45fca
Jayati Mishra <jmishra_be20@thapar.edu>
2022-01-18 12:38:06 +0530

    comments added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 def index(request) :
     
     return HttpResponse("Hello, world. You're at the polls index.")
+#This is the simplest view possible in Django
+#Tocall the view, we need to map to a URL and for this we need a URLConf
+#To create URLConf in the polls directory create a urls.py in the polls directory
